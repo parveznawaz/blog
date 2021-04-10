d26b30edbf57d69f1ffc9d1634cc3ca4f7895522
Parvez Nawaz <ParvezN@gs1ca.org>
2021-04-10 07:58:32 -0400

    kubernetes

--- moderation/index.js
+++ moderation/index.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', async (req, res) => {
-  
+
   const { type, data } = req.body;
   console.log(type, data)
   if (type === 'CommentCreated') {
     const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentModerated',
       data: {
         id: data.id,
